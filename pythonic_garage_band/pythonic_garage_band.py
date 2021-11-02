13f6d14b7cac342a970ae94c635b6329b90c70c7
Jana Abdelkader <janaosama1300@gmail.com>
2021-11-02 23:01:00 +0200

    skeleton for band class

---
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -0,0 +1,11 @@
+class band:
+    def __init__ (self, name, members):
+        self.name = name
+        self.members = []
+    
+    def play_solos():
+        pass
+
+    def to_list():
+        pass
+    # methods: play_solos +  to_list
